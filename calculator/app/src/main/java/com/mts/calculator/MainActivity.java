77afcff3e29992887e82dad0510c354e6992dfad
maitansang <40431247+maitansang@users.noreply.github.com>
2019-09-07 23:28:42 +0700

    Update MainActivity.java

--- calculator/app/src/main/java/com/mts/calculator/MainActivity.java
+++ calculator/app/src/main/java/com/mts/calculator/MainActivity.java
@@ -8,28 +8,61 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
-
 public class MainActivity extends AppCompatActivity {
-    EditText editTextA, editTextB;
-    TextView textView;
-    Button button;
+    private EditText mEditA, mEditB;
+    private TextView mTvResult;
+    private Button btn_plus, btn_sub, btn_mul, btn_div;
+    private int a, b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextA = (EditText) findViewById(R.id.edt_a);
-        editTextB = (EditText) findViewById(R.id.edt_b);
-        textView = (TextView) findViewById(R.id.tv_result);
-        button = (Button) findViewById(R.id.result);
-        button.setOnClickListener(new View.OnClickListener() {
+        mEditA = findViewById(R.id.edt_a);
+        mEditB = findViewById(R.id.edt_b);
+        btn_plus = findViewById(R.id.btn_plus);
+        btn_sub = findViewById(R.id.btn_sub);
+        btn_mul = findViewById(R.id.btn_mul);
+        btn_div = findViewById(R.id.btn_div);
+        mTvResult = findViewById(R.id.tv_result);
+        btn_plus.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                int so1 = Integer.parseInt(editTextA.getText().toString());
+            public void onClick(View v) {
+                a=getContentA();
+                b= getContentB();
+                String result=a+" + "+b+" = "+add(a,b);
+                mTvResult.setText(result);
+            }
+        });
 
-                int so2 = Integer.parseInt(editTextB.getText().toString());
-                textView.setText(String.valueOf(so1 + so2));
+        btn_sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a=getContentA();
+                b= getContentB();
+                String result=a+" - "+b+" = "+sub(a,b);
+                mTvResult.setText(result);
+            }
+        });
+
+        btn_mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a=getContentA();
+                b= getContentB();
+                String result=a+" * "+b+" = "+mul(a,b);
+                mTvResult.setText(result);
+            }
+        });
+
+        btn_div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a=getContentA();
+                b= getContentB();
+                String result;
+                    result= a + " / " +b+" = "+div(a,b);
+                mTvResult.setText(result);
             }
         });
     }
@@ -38,4 +71,24 @@ public class MainActivity extends AppCompatActivity {
         return a + b;
     }
 
+
+    int sub(int a, int b) {
+        return a - b;
+    }
+
+    int mul(int a, int b) {
+        return a * b;
+    }
+
+    float div(int a, int b) {
+        return (float) a / b;
+    }
+
+    int getContentA() {
+        return Integer.parseInt(mEditA.getText().toString());
+    }
+
+    int getContentB() {
+        return Integer.parseInt(mEditB.getText().toString());
+    }
 }
